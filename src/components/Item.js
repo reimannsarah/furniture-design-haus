53af5d471b0db280dc9510487b166b3ffde02b1a
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-28 14:37:58 -0700

    add class to name
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 function Item(props){
   return <div className="item-detail">
-    <p>{props.name}</p>
     <img src={props.src} alt="item detail"/>
+    <p className="item-detail-name">{props.name}</p>
     <p>{props.description}</p>
     <p>{props.price}</p>
   </div>
