d255d5d2d294a4b88d386e1cc8a3b0f79c0c295c
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-28 09:44:13 -0700

    add NavBar component
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div>
+      <p>Home</p>
+      <p>Contact</p>
+      <p>About</p>
+      <p>Shop</p>
+    </div>
+  )
+}
+
+export default NavBar;
