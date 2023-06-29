626348c5511b2dad885510c0de8e6159ac232ac7
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-29 11:38:58 -0700

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>&copy; Copyright 2023 Pancreas Designs, Sarah Reimann and Emma Gerigscott</p>
+    </div>
+  )
+}
+
+export default Footer;
