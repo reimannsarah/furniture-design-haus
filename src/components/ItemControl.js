da6983a168154616f97cfae3bca3229eb55f9fd6
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-29 11:40:16 -0700

    add buttons to delete, view cart, and add to cart
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/components/ItemControl.js
+++ src/components/ItemControl.js
@@ -1,7 +1,10 @@
 import React from "react";
+import NavBar from "./NavBar";
 import Item from "./Item";
 import ItemList from "./ItemList";
+import Cart from "./Cart";
 import allItems from "./store";
+import { v4 } from "uuid";
 
 
 class ItemControl extends React.Component {
@@ -11,19 +14,36 @@ class ItemControl extends React.Component {
     this.state = {
       detailVisibleOnPage: false,
       cart: [],
-      visibleItemIndex: 0
+      visibleItemIndex: 0,
+      cartVisibleOnPage: false,
     };
   }
 
-  handleAddingToCart = (item) => {
+  handleAddingToCart = (item) => { 
+    item.id = v4();
     const newCartItem = this.state.cart.concat(item);
-    this.setState({ cart: newCartItem })
+    this.setState({ cart: newCartItem,
+                  cartVisibleOnPage: true,
+                  detailVisibleOnPage: false
+                });
+    }
+
+  handleCartClick = () => {
+      this.setState({cartVisibleOnPage: true});
+    }
+  
+  handleDeleteFromCart = (id) => {
+    const newCart = this.state.cart.filter(item => item.id !== id)
+    this.setState({cart: newCart});
   }
 
   handleDetailClick = () => {
-    this.setState(prevState => ({
-      detailVisibleOnPage: !prevState.detailVisibleOnPage
-    }));
+    this.setState({detailVisibleOnPage: true});
+  }
+
+  handleReturnClick = () => {
+    this.setState({detailVisibleOnPage: false,
+                  cartVisibleOnPage: false})
   }
 
   handleNextItemClick = () => {
@@ -49,16 +69,26 @@ class ItemControl extends React.Component {
   render() {
     let currentlyVisibleState = null;
     let currentItem = allItems[this.state.visibleItemIndex]
-    if (this.state.detailVisibleOnPage) {
+    
+    if (this.state.cartVisibleOnPage) {
+      currentlyVisibleState = 
+      <React.Fragment>
+        <Cart 
+          cart={this.state.cart} 
+          deleteItem={this.handleDeleteFromCart} 
+        />
+      </React.Fragment>
+    } else if (this.state.detailVisibleOnPage) {
       currentlyVisibleState = 
       <div className="item-detail">
-        <button className="x" onClick={this.handleDetailClick}>&#10554;</button>
+        <button className="x" onClick={this.handleReturnClick}>&#10554;</button>
         <Item
           name={currentItem.name}
           src={currentItem.src}
           description={currentItem.description}
           price={currentItem.price}
         />
+        <button className="add-to-cart-btn" onClick={() => {this.handleAddingToCart(currentItem)}}>Add to Cart</button>
       </div>
     } else {
       currentlyVisibleState =
@@ -72,6 +102,7 @@ class ItemControl extends React.Component {
     }
     return (
       <React.Fragment>
+        <NavBar onHomeClick={this.handleReturnClick} onCartClick={this.handleCartClick} />
         {currentlyVisibleState}
       </React.Fragment>
     )
