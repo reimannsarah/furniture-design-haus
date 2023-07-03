027603fa2cb13bb6ee2d8856a47134478054d6dc
Emma Gerigscott <elgerig@gmail.com>
2023-07-03 09:25:09 -0700

    redux action add item + tests passing

--- src/__tests__/reducers/item-list-resucer.test.js
+++ src/__tests__/reducers/item-list-resucer.test.js
@@ -1,7 +1,38 @@
 import itemListReducer from '../../reducers/item-list-reducer';
 
 describe('itemListReducer', () => {
+
+  let action;
+  const itemData = {
+    name: 'scorcher',
+    src: 'picture',
+    description: 'a hot bench',
+    price: '$456',
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(itemListReducer({}, { type: null })).toEqual({});
   });
-})
+
+  test('Should successfully add new item data to mainItemList', () => {
+    const { name, src, description, price, id } = itemData;
+    action = {
+      type: 'ADD_ITEM',
+      name: name,
+      src: src,
+      description: description,
+      price: price,
+      id: id
+    };
+    expect(itemListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        src: src,
+        description: description,
+        price: price,
+        id: id
+      }
+    });
+  });
+});
--- src/reducers/item-list-reducer.js
+++ src/reducers/item-list-reducer.js
@@ -1,5 +1,19 @@
 const reducer = (state = {}, action) => {
-  return state;
+  const { name, src, description, price, id } = action;
+  switch(action.type) {
+    case 'ADD_ITEM':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          src: src,
+          description: description,
+          price: price,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
 
 export default reducer;
