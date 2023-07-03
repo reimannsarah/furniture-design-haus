3dfffc0d79cc7fd1507f2dd011e30e2f3631f9fa
Emma Gerigscott <elgerig@gmail.com>
2023-07-03 09:31:45 -0700

    config redux in index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { configureStore } from 'redux';
+import reducer from './reducers/item-list-reducer';
+import { Provider } from 'react-redux';
+
+const store = configureStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
